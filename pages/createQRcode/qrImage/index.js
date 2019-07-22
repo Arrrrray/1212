0de0109683edf1eca1c34b0cb0fc033b786016a1
Arrrrray <junchaocode@gmail.com>
2019-07-22 17:53:28 +0800

    edit qecode

--- pages/createQRcode/qrImage/index.js
+++ pages/createQRcode/qrImage/index.js
@@ -1,22 +1,20 @@
-// pages/createQRcode/index.js
-import util from '../../utils/util.js';
+// pages/createQRcode/qrImage/index.js
+import util from '../../../utils/util.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // 二维码的内容
-    text: '',
-    // 输入的字符长度
-    text_length: 0,
+    // 生成给base64二维码地址
+    qrcodesrc: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    this.createQRcode(options.text)
   },
 
   /**
@@ -67,37 +65,11 @@ Page({
   onShareAppMessage: function() {
 
   },
-
-  // 用户输入的内容
-  inputTextChange: function(e) {
-    this.setData({
-      text: e.detail.value,
-      text_length: e.detail.cursor,
-    })
-  },
-
   // 生成二维码
-  createQRcode: function() {
-    const {
-      text,
-      text_length,
-    } = this.data;
-    if (text_length === 0) {
-      wx.showModal({
-        title: '提示',
-        content: '请输入想要生成的二维码的内容',
-        showCancel: false,
-        success(res) {
-          if (res.confirm) {
-            console.log('用户点击确定')
-            return
-          }
-        }
-      })
-      return
-    }
-    wx.redirectTo({
-      url: `/pages/createQRcode/qrImage/index?text=${text}`,
+  createQRcode: function(text) {
+    let qrcodesrc = util.createBase64QRcode(text);
+    this.setData({
+      qrcodesrc,
     })
   },
 })
